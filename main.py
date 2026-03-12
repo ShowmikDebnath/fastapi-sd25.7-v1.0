85a459974c2c8e85dc7bdbddaab499ce7f26d7d8
ShowmikDebnath <67783022+ShowmikDebnath@users.noreply.github.com>
2026-03-12 11:50:58 +0600

    Pydantic and Fetching all products

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -8,7 +9,21 @@ def greet():
     return "Welcome to my server!"
 
 
+# product = [
+#     Product(1, "Phone", "Budget Phone", 99, 10),
+#     Product(2, "Laptop", "Gaiming laptop", 999, 6)
+# ]
 
+products = [
+    Product(id=1, name="Phone", description="A smartphone", price=99.98, quantity=50),
+    Product(id=2, name="Laptop", description="Gaiming laptop", price=699.98, quantity=10),
+    Product(id=3, name="Pen", description="A blue ink pen", price=10.98, quantity=100),
+    Product(id=4, name="Table", description="A wooden table", price=196.10, quantity=5)
+]
+
+@app.get("/products")
+def get_all_products():
+    return products
 
 
 
