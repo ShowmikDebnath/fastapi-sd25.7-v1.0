a0cfb10baf0af26b2344a63c2bc6553a9c548b5f
ShowmikDebnath <67783022+ShowmikDebnath@users.noreply.github.com>
2026-03-14 20:52:04 +0600

    SQLAlchemy Model Creation

--- database.py
+++ database.py
@@ -5,3 +5,4 @@ from sqlalchemy.orm import sessionmaker
 db_url = "postgresql://showmik@localhost:5432/showmik25.7_v1.0"
 engine = create_engine(db_url)
 session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
